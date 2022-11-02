5b73c859eb4f96e9f818402a747ccf41d3db01ac
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-11-03 00:14:41 +0530

    Add project files.

---
+++ SpanVsString/StringParser.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class StringParser
+{
+    public string GetFirstName(string fullName)
+    {
+        return fullName.Split(" ")[0] ?? string.Empty;
+    }
+
+    public string GetFirstNameUsingSubstring(string fullName)
+    {
+        var lastSpaceIndex = fullName.LastIndexOf(" ", StringComparison.Ordinal);
+
+        return lastSpaceIndex == -1
+            ? string.Empty
+            : fullName.Substring(0, lastSpaceIndex);
+    }
+
+    public ReadOnlySpan<char> GetFirstNameWithSpan(ReadOnlySpan<char> fullName)
+    {
+        var lastSpaceIndex = fullName.LastIndexOf(' ');
+
+        return lastSpaceIndex == -1
+            ? ReadOnlySpan<char>.Empty
+            : fullName.Slice(0, lastSpaceIndex);
+    }
+}
---
+++ SpanVsString/StringParserBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+[RankColumn]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[MemoryDiagnoser]
+public class StringParserBenchmarks
+{
+    private const string FullName = "Abdul Rahman";
+    private static readonly StringParser Parser = new();
+
+    [Benchmark(Baseline = true)]
+    public void GetFirstName()
+    {
+        Parser.GetFirstName(FullName);
+    }
+
+    [Benchmark]
+    public void GetFirstNameUsingSubstring()
+    {
+        Parser.GetFirstNameUsingSubstring(FullName);
+    }
+
+    [Benchmark]
+    public void GetFirstNameWithSpan()
+    {
+        Parser.GetFirstNameWithSpan(FullName);
+    }
+}
